7802ca269b9f318c65b0d6bdb51b1f752da31a4e
kakule <sinkari.kassim@gmail.com>
2016-11-07 21:29:30 -0800

    minor change

--- app/src/main/java/com/codepath/apps/simpletwitterclient/activities/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/simpletwitterclient/activities/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Toast;
 
 import com.codepath.apps.simpletwitterclient.R;
 import com.codepath.apps.simpletwitterclient.clients.TwitterClient;
@@ -33,7 +32,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	public void onLoginSuccess() {
 		Intent i = new Intent(this, TimelineActivity.class);
 		startActivity(i);
-        Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
 	}
 
 	// OAuth authentication flow failed, handle the error
